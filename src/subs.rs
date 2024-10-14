f3f50db1c24471e53590d53ae544509ad0b4b8a3
Zedjones <dojoman19@gmail.com>
2024-10-13 23:00:46 -0400

    Fixed math bug in reference line calculation

--- src/subs.rs
+++ src/subs.rs
@@ -10,11 +10,13 @@ pub(crate) struct Subs {
 
 impl Subs {
     pub fn choices_for_timecode(&self, timecode: &Duration) -> Vec<ASSEvent> {
-        let timecode_as_moment = Moment::from(timecode.as_millis() as i64);
+        let start = Moment::from(((timecode.as_millis() / 1000) * 1000) as i64);
+        let end = Moment::from(((timecode.as_millis() / 1000 + 1) * 1000) as i64);
         self.subtitle_file
             .events()
             .into_iter()
-            .filter(|&event| event.start < timecode_as_moment && timecode_as_moment < event.end)
+            .filter(|&event| event.start < end && event.end > start)
+            .filter(|&event| !event.text.contains("\\pos("))
             .map(|event| event.into())
             .collect()
     }
